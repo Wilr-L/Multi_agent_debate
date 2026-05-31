a8099c2fb37778a86e52e35cdc39b4fb5bf082f2
Wilr <yisiwu95@gmail.com>
2026-05-31 01:55:28 +0100

    3debate5

--- Three_debate/sbatch2_kcl.sh
+++ Three_debate/sbatch2_kcl.sh
@@ -6,10 +6,12 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=256G
 #SBATCH --nodes=1
+#SBATCH -p gpu
+#SBATCH --constraint=a100
 #SBATCH --gres=gpu:4 #根据需要设置，只使用CPU此行省略
- 
-source /apps/soft/anaconda3/bin/activate
-conda activate embodi
+
+source activate /scratch/users/k25159491/.conda/envs/embodi2
+module load cuda
 python /scratch/users/k25159491/WORK/Multi_agent_debate/Three_debate/evaluate_viki_l2_localmodel.py \
  --model-path /scratch/prj/inf_du/k25159491/Model/Qwen2.5-VL-32B-Instruct \
  --max-model-len 12864  --gpu-mem-util 0.85  --trust-remote-code --tensor-parallel-size 4
